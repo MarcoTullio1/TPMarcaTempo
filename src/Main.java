156112992732217b6915d94f619ec2a61389ba36
Marco Tullio <64559501+MarcoTullio1@users.noreply.github.com>
2023-04-01 22:54:17 -0300

    Implementação do selectionSort

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Random;
 
@@ -8,16 +7,14 @@ public class Main {
         int[] vetorPrincipal =  geraVetor(100000);
         int[] vetorCopia =  vetorPrincipal.clone();
 
-
+        System.out.println("Bolha:");
+        bubbleSort(vetorCopia);
+        System.out.println("Selecaao:");
+        selectionSort(vetorCopia);
 
        // Arrays.stream(geraVetor(100000)).forEach(z-> System.out.println(z));
     }
 
-    static void trocar(int[] dados, int pos1, int pos2) {
-        int aux = dados[pos1];
-        dados[pos1] = dados[pos2];
-        dados[pos2] = aux;
-    }
     public static int[] geraVetor(int qtdValores) {
 
         int[] vetor = new int[qtdValores];
@@ -37,4 +34,48 @@ public class Main {
         return vetor;
     }
 
+    public static void bubbleSort(int[] vetor) {
+        int n = vetor.length;
+        int comparacoes = 0;
+        int trocas = 0;
+
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                comparacoes++;
+                if (vetor[j] > vetor[j+1]) {
+                    int temp = vetor[j];
+                    vetor[j] = vetor[j+1];
+                    vetor[j+1] = temp;
+                    trocas++;
+                }
+            }
+        }
+
+        System.out.println("Operações de comparação: " + comparacoes);
+        System.out.println("Operações de troca: " + trocas);
+    }
+
+    public static void selectionSort(int[] vetor) {
+        int n = vetor.length;
+        int comparacoes = 0;
+        int trocas = 0;
+
+        for (int i = 0; i < n-1; i++) {
+            int min_idx = i;
+            for (int j = i+1; j < n; j++) {
+                comparacoes++;
+                if (vetor[j] < vetor[min_idx]) {
+                    min_idx = j;
+                }
+            }
+
+            int temp = vetor[min_idx];
+            vetor[min_idx] = vetor[i];
+            vetor[i] = temp;
+            trocas++;
+        }
+
+        System.out.println("Operações de comparação: " + comparacoes);
+        System.out.println("Operações de troca: " + trocas);
+    }
 }
