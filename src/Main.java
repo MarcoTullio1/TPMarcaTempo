8a58031d15880777edb5b1502e5139cb76c73093
Marco Tullio <64559501+MarcoTullio1@users.noreply.github.com>
2023-04-02 14:41:35 -0300

    Parte 1 finalizada

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,87 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Random;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
 
-        int[] vetorPrincipal =  geraVetor(100000);
-        int[] vetorCopia =  vetorPrincipal.clone();
+        int numExecucoes = 50;
+        int tamanhoVetor = 100000;
+        long[] compBubble = new long[numExecucoes];
+        long[] trocasBubble = new long[numExecucoes];
+        long[] compSelection = new long[numExecucoes];
+        long[] trocasSelection = new long[numExecucoes];
 
-        System.out.println("Bolha:");
-        bubbleSort(vetorCopia);
-        System.out.println("Selecaao:");
-        selectionSort(vetorCopia);
+        try {
+            FileWriter fw = new FileWriter("resultados.txt");
+            BufferedWriter bw = new BufferedWriter(fw);
 
-       // Arrays.stream(geraVetor(100000)).forEach(z-> System.out.println(z));
-    }
+            bw.write("Resultados da ordenação de vetores de " + tamanhoVetor + " elementos\n\n");
 
-    public static int[] geraVetor(int qtdValores) {
+
+            for (int i = 0; i < numExecucoes; i++) {
+            System.out.println(i);
+            int[] vetor = geraVetor(tamanhoVetor);
+            int[] copia = vetor.clone();
+
+            // Ordena o vetor com o algoritmo Bubble Sort e registra o número de comparações e trocas
+            long[] resultBubble = bubbleSort(vetor);
+            compBubble[i] = resultBubble[0];
+            trocasBubble[i] = resultBubble[1];
+
+            // Ordena a cópia do vetor com o algoritmo Selection Sort e registra o número de comparações e trocas
+            long[] resultSelection = selectionSort(copia);
+            compSelection[i] = resultSelection[0];
+            trocasSelection[i] = resultSelection[1];
+        }
+
+        // Calcula as estatísticas das execuções
+        long maxCompBubble = calcularMax(compBubble);
+        long minCompBubble = calcularMin(compBubble);
+        long mediaCompBubble = calcularMedia(compBubble);
+        long maxTrocasBubble = calcularMax(trocasBubble);
+        long minTrocasBubble = calcularMin(trocasBubble);
+        long mediaTrocasBubble = calcularMedia(trocasBubble);
+        long maxCompSelection = calcularMax(compSelection);
+        long minCompSelection = calcularMin(compSelection);
+        long mediaCompSelection = calcularMedia(compSelection);
+        long maxTrocasSelection = calcularMax(trocasSelection);
+        long minTrocasSelection = calcularMin(trocasSelection);
+        long mediaTrocasSelection = calcularMedia(trocasSelection);
+
+        // Exibe os resultados
+        bw.write("Estatísticas do Bubble Sort:");
+        bw.write("\nMaior quantidade de comparações: " + maxCompBubble);
+        bw.write("\nMenor quantidade de comparações: " + minCompBubble);
+        bw.write("\nMédia de comparações: " + mediaCompBubble);
+        bw.write("\nMaior quantidade de trocas: " + maxTrocasBubble);
+        bw.write("\nMenor quantidade de trocas: " + minTrocasBubble);
+        bw.write("\nMédia de trocas: " + mediaTrocasBubble +"\n");
+
+        bw.write("\nEstatísticas do Selection Sort:");
+        bw.write("\nMaior quantidade de comparações: " + maxCompSelection);
+        bw.write("\nMenor quantidade de comparações: " + minCompSelection);
+        bw.write("\nMédia de comparações: " + mediaCompSelection);
+        bw.write("\nMaior quantidade de trocas: " + maxTrocasSelection);
+        bw.write("\nMenor quantidade de trocas: " + minTrocasSelection);
+        bw.write("\nMédia de trocas: " + mediaTrocasSelection);
+
+            bw.close();
+            fw.close();
+            System.out.println("Resultados salvos no arquivo resultados.txt");
+    } catch (IOException e) {
+            System.out.println("Erro ao escrever no arquivo: " + e.getMessage());
+        }        }
+
+
+
+    // Arrays.stream(geraVetor(100000)).forEach(z-> System.out.println(z));
+
+
+    private static int[] geraVetor(int qtdValores) {
 
         int[] vetor = new int[qtdValores];
         int randomNumber ;
@@ -34,48 +100,75 @@ public class Main {
         return vetor;
     }
 
-    public static void bubbleSort(int[] vetor) {
-        int n = vetor.length;
-        int comparacoes = 0;
-        int trocas = 0;
+    public static long[] bubbleSort(int[] arr) {
+        int n = arr.length;
+        long comparacoes = 0;
+        long trocas = 0;
 
         for (int i = 0; i < n-1; i++) {
             for (int j = 0; j < n-i-1; j++) {
                 comparacoes++;
-                if (vetor[j] > vetor[j+1]) {
-                    int temp = vetor[j];
-                    vetor[j] = vetor[j+1];
-                    vetor[j+1] = temp;
+                if (arr[j] > arr[j+1]) {
                     trocas++;
+                    // swap arr[j+1] and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
         }
 
-        System.out.println("Operações de comparação: " + comparacoes);
-        System.out.println("Operações de troca: " + trocas);
+        return new long[] {comparacoes, trocas};
     }
 
-    public static void selectionSort(int[] vetor) {
-        int n = vetor.length;
-        int comparacoes = 0;
-        int trocas = 0;
+    public static long[] selectionSort(int[] arr) {
+        int n = arr.length;
+        long comparacoes = 0;
+        long trocas = 0;
 
         for (int i = 0; i < n-1; i++) {
             int min_idx = i;
             for (int j = i+1; j < n; j++) {
                 comparacoes++;
-                if (vetor[j] < vetor[min_idx]) {
+                if (arr[j] < arr[min_idx]) {
                     min_idx = j;
                 }
             }
-
-            int temp = vetor[min_idx];
-            vetor[min_idx] = vetor[i];
-            vetor[i] = temp;
             trocas++;
+            // swap arr[min_idx] and arr[i]
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
         }
 
-        System.out.println("Operações de comparação: " + comparacoes);
-        System.out.println("Operações de troca: " + trocas);
+        return new long[] {comparacoes, trocas};
+    }
+
+    private static long calcularMax(long[] valores) {
+        long max = Long.MIN_VALUE;
+        for (long valor : valores) {
+            if (valor > max) {
+                max = valor;
+            }
+        }
+        return max;
+    }
+
+    private static long calcularMin(long[] valores) {
+        long min = Long.MAX_VALUE;
+        for (long valor : valores) {
+            if (valor < min) {
+                min = valor;
+            }
+        }
+        return min;
+    }
+
+    private static long calcularMedia(long[] valores) {
+        long soma = 0;
+        for (long valor : valores) {
+            soma += valor;
+        }
+        return soma / valores.length;
     }
 }
